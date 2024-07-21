27cf1b5f5834802c46cd2579f8ec235b9003b0c6
Rajesh B <rajesh.mbalu@gmail.com>
2024-07-21 23:48:57 +0530

    skills update

--- src/skill/Skill.jsx
+++ src/skill/Skill.jsx
@@ -36,45 +36,45 @@ const Skill = () => {
     return (
         <>
             <h1 data-aos="fade-up" className="title text-titleLight dark:text-titleDark text-[2.4rem] mt-[8%] ml-[30%] md:mt-0  md:ml-[43%]" id="skills">My <span className='text-[2.4rem] text-baseColor'>Skills</span></h1>
-                    <div data-aos="fade-up" className="content w-full max-w-screen-xl mx-auto">
-                        <Slider {...settings} >
-                            <div className="p-4">
-                                <div className="rounded-lg text-contentLight dark:text-contentDark bg-secLight dark:bg-secDark p-6">
-                                    <h1 className="text-2xl font-bold text-center text-baseColor">Languages</h1>
-                                    <h1 className="mt-2 text-xl text-center">Python</h1>
-                                    <h1 className="text-xl text-center">JavaScript</h1>
-                                </div>
-                            </div>
-                            <div className="p-4">
-                                <div className="rounded-lg text-contentLight dark:text-contentDark bg-secLight dark:bg-secDark p-6">
-                                    <h1 className="text-2xl font-bold text-center text-baseColor">Full Stack</h1>
-                                    <h1 className="mt-2 text-xl text-center"><span className="text-xl font-bold text-center">Frontend : </span>React.JS, Angular.JS</h1>
-                                    <h1 className="text-xl text-center"><span className="text-xl font-bold text-center">Backend : </span>Django</h1>
-                                </div>
-                            </div>
-                            <div className="p-4">
-                                <div className="rounded-lg text-contentLight dark:text-contentDark bg-secLight dark:bg-secDark p-6">
-                                    <h1 className="text-2xl font-bold text-center text-baseColor">Web Technologies</h1>
-                                    <h1 className="mt-2 text-xl text-center">HTML5</h1>
-                                    <h1 className="text-xl text-center">CSS3</h1>
-                                </div>
-                            </div>
-                            <div className="p-4">
-                                <div className="rounded-lg text-contentLight dark:text-contentDark bg-secLight dark:bg-secDark p-6">
-                                    <h1 className="text-2xl font-bold text-center text-baseColor">CSS Framework</h1>
-                                    <h1 className="mt-2 text-xl text-center">Tailwind CSS</h1>
-                                    <h1 className="text-xl text-center">Bootstrap</h1>
-                                </div>
-                            </div>
-                            <div className="p-4">
-                                <div className="rounded-lg text-contentLight dark:text-contentDark bg-secLight dark:bg-secDark p-6">
-                                    <h1 className="text-2xl font-bold text-center text-baseColor">Scripting Languages</h1>
-                                    <h1 className="mt-2 text-xl text-center">Ajax</h1>
-                                    <h1 className="text-xl text-center">jQuery</h1>
-                                </div>
-                            </div>
-                        </Slider>
+            <div data-aos="fade-up" className="content w-full max-w-screen-xl mx-auto">
+                <Slider {...settings} >
+                    <div className="p-4">
+                        <div className="rounded-lg text-contentLight dark:text-contentDark bg-secLight dark:bg-secDark p-6">
+                            <h1 className="text-2xl font-bold text-center text-baseColor">Languages</h1>
+                            <h1 className="mt-2 text-xl text-center">Python</h1>
+                            <h1 className="text-xl text-center">JavaScript</h1>
+                        </div>
                     </div>
+                    <div className="p-4">
+                        <div className="rounded-lg text-contentLight dark:text-contentDark bg-secLight dark:bg-secDark p-6">
+                            <h1 className="text-2xl font-bold text-center text-baseColor">Full Stack</h1>
+                            <h1 className="mt-2 text-xl text-center">React.JS, Angular.JS</h1>
+                            <h1 className="text-xl text-center">Django</h1>
+                        </div>
+                    </div>
+                    <div className="p-4">
+                        <div className="rounded-lg text-contentLight dark:text-contentDark bg-secLight dark:bg-secDark p-6">
+                            <h1 className="text-2xl font-bold text-center text-baseColor">Web Technologies</h1>
+                            <h1 className="mt-2 text-xl text-center">HTML5</h1>
+                            <h1 className="text-xl text-center">CSS3</h1>
+                        </div>
+                    </div>
+                    <div className="p-4">
+                        <div className="rounded-lg text-contentLight dark:text-contentDark bg-secLight dark:bg-secDark p-6">
+                            <h1 className="text-2xl font-bold text-center text-baseColor">CSS Framework</h1>
+                            <h1 className="mt-2 text-xl text-center">Tailwind CSS</h1>
+                            <h1 className="text-xl text-center">Bootstrap</h1>
+                        </div>
+                    </div>
+                    <div className="p-4">
+                        <div className="rounded-lg text-contentLight dark:text-contentDark bg-secLight dark:bg-secDark p-6">
+                            <h1 className="text-2xl font-bold text-center text-baseColor">Scripting Languages</h1>
+                            <h1 className="mt-2 text-xl text-center">Ajax</h1>
+                            <h1 className="text-xl text-center">jQuery</h1>
+                        </div>
+                    </div>
+                </Slider>
+            </div>
         </>
     );
 };
