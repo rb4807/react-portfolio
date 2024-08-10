30594358060487e36deaede95090b0d5d77ded3e
Rajesh B <rajesh.mbalu@gmail.com>
2024-08-10 13:07:42 +0530

    Update Certification.jsx

--- src/certification/Certification.jsx
+++ src/certification/Certification.jsx
@@ -2,9 +2,9 @@ const Certification = () => {
 
     return (
         <>
-            <h1 data-aos="fade-up" className="title text-titleLight dark:text-titleDark text-[2.4rem] mt-[20%] ml-[16%] md:mt-[15%] md:ml-[43%]" id="certi">My <span className='text-[2.4rem] text-baseColor'>Certifications</span></h1>
-            <div data-aos="fade-up" className="container mx-auto p-4">
-                <a href='https://certifications-ashen.vercel.app/' target='blank'>
+            <h1 data-aos="fade-up" className="title text-titleLight dark:text-titleDark text-[2.4rem] mt-[10%] ml-[10%] md:mt-[8%] md:ml-[25%]" id="certi">My <span className='text-[2.4rem] text-baseColor'>Certifications</span></h1>
+            <div data-aos="fade-up" className="container mx-auto p-4 max-w-4xl">
+                <a href='https://certifications-ashen.vercel.app/' target='_blank'>
                     <div className="content bg-secLight dark:bg-secDark text-contentLight dark:text-contentDark p-4 rounded-lg text-[.80rem] hover:scale-105 duration-1000 transform md:text-[1.2rem]">
                         • Power Programmer Virtual Experience Program from <span className="ml-[2.7%] text-baseColor md:ml-[0%]">Infosys.</span> <br/>
                         • <span className="text-baseColor">Python Backend And Django</span> Framework Complete Course.<br/>
@@ -16,9 +16,9 @@ const Certification = () => {
                 </a>
             </div>
 
-            <div className="flex flex-wrap justify-center">
+            <div className="flex flex-wrap justify-center mt-4">
                 <a href ="#top"> 
-                    <button className="px-10 py-2 rounded-lg  bg-baseColor hover:bg-titleLight text-titleDark hover:text-white dark:bg-baseColor dark:hover:bg-titleDark dark:text-titleDark dark:hover:text-black hover:border-transparent">Back to top
+                    <button className="px-10 py-2 rounded-lg bg-baseColor hover:bg-titleLight text-titleDark hover:text-white dark:bg-baseColor dark:hover:bg-titleDark dark:text-titleDark dark:hover:text-black hover:border-transparent">Back to top
                         <svg xmlns="http://www.w3.org/2000/svg" className="inline-block icon icon-tabler icon-tabler-chevrons-up" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                             <path d="M7 11l5 -5l5 5" />
@@ -32,4 +32,3 @@ const Certification = () => {
 };
 
 export default Certification;
-
