f50b0bcc938770e0e996daa323d697df0fa3b0e7
Rajesh B <117339281+rb4807@users.noreply.github.com>
2024-10-05 20:39:34 +0530

    Update Service.jsx

--- src/service/Service.jsx
+++ src/service/Service.jsx
@@ -6,6 +6,7 @@ const Service = () => {
             <div data-aos="fade-up" className="container mx-auto p-4">
                 <div className="content grid md:grid-cols-3 grid-cols-2 gap-4 text-contentLight dark:text-contentDark">
                     <div className="bg-secLight hover:scale-105 duration-1000 transform dark:bg-secDark p-4 rounded-lg text-center shadow shadow-baseColor">Full Stack Development</div>
+                    <div className="bg-secLight hover:scale-105 duration-1000 transform dark:bg-secDark p-4 rounded-lg text-center shadow shadow-baseColor">Software Development</div>
                     <div className="bg-secLight hover:scale-105 duration-1000 transform dark:bg-secDark p-4 rounded-lg text-center shadow shadow-baseColor">Python Projects</div>
                     <div className="bg-secLight hover:scale-105 duration-1000 transform dark:bg-secDark p-4 rounded-lg text-center shadow shadow-baseColor">Api Integration</div>
                     <div className="bg-secLight hover:scale-105 duration-1000 transform dark:bg-secDark p-4 rounded-lg text-center shadow shadow-baseColor">ETL</div>
