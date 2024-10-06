8b1de8e0e028ba9ca9c28d110560031fb26665b4
Rajesh B <rajesh.mbalu@gmail.com>
2024-10-06 15:25:50 +0530

    Update Certification.jsx

--- src/certification/Certification.jsx
+++ src/certification/Certification.jsx
@@ -1,8 +1,8 @@
 const Certification = () => {
     return (
-        <div className="mt-[22%] md:mt-[14%]">
+        <div className="mt-[22%] md:mt-[14%]" id="certfication">
             <h1 data-aos="fade-up" className="title text-titleLight dark:text-titleDark text-[2rem] md:text-[2.4rem] text-center" id="certi">My <span className='text-baseColor'>Certifications</span></h1>
-            <div data-aos="fade-up" id="certfication" className="container mx-auto p-4 max-w-4xl">
+            <div data-aos="fade-up" className="container mx-auto p-4 max-w-4xl">
                 <a href='https://certifications-ashen.vercel.app/' target='_blank' rel="noopener noreferrer">
                     <div className="content bg-secLight dark:bg-secDark text-contentLight dark:text-contentDark p-4 rounded-lg text-[0.8rem] md:text-[1rem] lg:text-[1.2rem] hover:scale-105 duration-300 transform">
                         <ul className="list-disc pl-5">
